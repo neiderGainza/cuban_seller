2d3371017ae785950a1c4ce6c451d93ef31504d9
Karel <karel.duransocarras0@gmail.com>
2025-12-10 09:06:30 -0500

    feat: Refactor CoinMapper to improve entity mapping methods

--- lib/features/account/data/mapper/coin_mapper.dart
+++ lib/features/account/data/mapper/coin_mapper.dart
@@ -1,6 +1,18 @@
 import 'package:cuban_seller/features/account/data/datasources/local/app_database.dart';
 import 'package:cuban_seller/features/account/domain/entities/coin.dart';
+import 'package:cuban_seller/features/account/domain/usecases/insert_coin_usecases.dart';
+import 'package:drift/drift.dart';
 
 class CoinMapper {
-  static Coin toMap(CoinModelData model) => Coin(value: model.value);
+  static Coin toEntity(CoinModelData row) {
+    return Coin(value: row.value);
+  }
+
+  static CoinModelCompanion toCompanionFromParam(CoinParameters params) {
+    return CoinModelCompanion(value: Value(params.value));
+  }
+
+  static Coin toEntityFromParam(CoinParameters params) {
+    return Coin(value: params.value);
+  }
 }
